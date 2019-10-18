909b151b0bfd9446a230309ddb5e473aa1042cf4
keshareenv <kesharee.nv@unbxd.com>
2019-10-18 17:28:30 +0530

    fix(scripts): fix version replacement

--- scripts/push_to_aws.sh
+++ scripts/push_to_aws.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# remove v or V from the tag if present
-VERSION=${TRAVIS_TAG#(v|V)}
+# remove v from the tag if present
+VERSION=${TRAVIS_TAG//v/}
 
 echo "pushing solr package to s3"
 
-aws s3 cp -r package s3://${S3_PATH}/solr/dist/$VERSION
+aws s3 cp $TRAVIS_BUILD_DIR/solr/package s3://${S3_PATH}/solr/dist/$VERSION --recursive
 
 echo "successfully pushed package to s3"
