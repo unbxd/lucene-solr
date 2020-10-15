ec3c869ee03b1c015baa49db2c6d54e9fb4191da
S N Munendra <munendra@unbxd.com>
2020-10-15 12:07:33 +0530

    fix: change env variable name in startup script

--- solr/script/start.sh
+++ solr/script/start.sh
@@ -2,20 +2,19 @@
 
 cmd="solr -f"
 
-if [ ! -z "$CLOUD" ]; then
+if [ ! -z "$CLOUD" ]; then 
     if [ "$CLOUD" == "yes" ]; then
-        [ -z "$ZOOK_SERVERS" ] && {
-            echo "Can't start Solr. ZOOK_SERVERS missing. Aborting";
+        [ -z "$ZK_HOST" ] && {
+            echo "Can't start Solr. ZK_HOST missing. Aborting";
             exit 1;
         }
         cmd="$cmd -c"
-        cmd="$cmd -z $ZOOK_SERVERS"
+        cmd="$cmd -z $ZK_HOST"
     fi
 fi
 
 
 # Check for other env vars
-[ ! -z "$SOLR_HOST" ] && { cmd="$cmd -Dhost=$SOLR_HOST"; }
 [ ! -z "$ASSET_ENDPOINT" ] && { cmd="$cmd -DassetEndpoint=$ASSET_ENDPOINT"; }
 [ ! -z "$JAVA_OPTS" ] && { cmd="$cmd -a \"$JAVA_OPTS\""; }
 
